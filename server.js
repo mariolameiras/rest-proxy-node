8f517d36d6b366a6d4e7d966c66b7f1cbf165af4
Mario Lameiras <mario.lameiras@gmail.com>
2015-02-22 18:46:58 +0000

    very simple proxy for restful services, hardcoded url

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+//var bitreserve = require('./client');
+var Client = require('node-rest-client').Client;
+
+var app = express();
+var port = process.env.PORT || 8080;
+var router = express.Router();
+var	client = new Client();
+
+
+router.use(function(req, res, next) {
+	console.log('%s %s', req.method, req.path);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();  
+});
+
+router.get('/bitreserve', function(req, res, next) {
+	client.get("https://api.bitreserve.org/v0/reserve/statistics", function(data, response){
+		res.json(JSON.parse(data));
+	});
+});
+
+
+// Only requests to /api/ will be send to router.
+app.use('/api', router);
+app.listen(port);
+console.log('Server listening on port ' + port);
+
+
